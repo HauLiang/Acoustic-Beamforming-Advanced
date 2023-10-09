bf4f77398c8c9dad10c8b48fd1ff39cbe4078224
Hao Liang <haoliang2023@hotmail.com>
2023-10-09 19:25:01 +0800

    Update lipschitz.m

--- Toolbox/lipschitz.m
+++ Toolbox/lipschitz.m
@@ -1,6 +1,17 @@
 function L = lipschitz(PSF, fft_psf, D, lambda)
+% Calculate Lipschitz constant for FFT-DFISTA algorithm
+%
+% Input:
+%   PSF :  point spread function
+%   fft_psf: point spread function (PSF) after Fourier transform
+%   D:     modified first-order difference matrix
+%   lambda: penalty parameter
+%
+% Output: 
+%   L:  estimated Lipschitz constant
+%
 
-% Calculate Lipschitz constant by power iteration for FFT-DFISTA algorithm
+% Using power iteration
 xx = rand(size(PSF));
 for k = 1:10
     xx = (fftshift(ifft2(fft2(xx).*fft_psf))+lambda*D*xx)/norm(xx,'fro');
