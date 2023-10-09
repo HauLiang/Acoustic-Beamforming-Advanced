f3cf63637e1aa37d45e43fd79004d8b9cac3c09e
Hao Liang <haoliang2023@hotmail.com>
2023-10-09 19:21:45 +0800

    Update lipschitz.m

--- Toolbox/lipschitz.m
+++ Toolbox/lipschitz.m
@@ -1,6 +1,6 @@
 function L = lipschitz(PSF, fft_psf, D, lambda)
 
-% Estimate Lipschitz constant by power iteration
+% Calculate Lipschitz constant by power iteration for FFT-DFISTA algorithm
 xx = rand(size(PSF));
 for k = 1:10
     xx = (fftshift(ifft2(fft2(xx).*fft_psf))+lambda*D*xx)/norm(xx,'fro');
