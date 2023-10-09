03a0e2d88e6286d481e3c415ca197d35c8d67178
Hao Liang <haoliang2023@hotmail.com>
2023-10-09 19:18:40 +0800

    Update lipschitz.m

--- Toolbox/lipschitz.m
+++ Toolbox/lipschitz.m
@@ -1,11 +1,11 @@
-function L = lipschitz(PSF, fft_psf)
+function L = lipschitz(PSF, fft_psf, D, lambda)
 
 % Estimate Lipschitz constant by power iteration
 xx = rand(size(PSF));
 for k = 1:10
-    xx = fftshift(ifft2(fft2(xx).*fft_psf))/norm(xx,'fro');
+    xx = (fftshift(ifft2(fft2(xx).*fft_psf))+lambda*D*xx)/norm(xx,'fro');
 end
 
-L = norm(xx,'fro')^2;    % lambda(A'A) Assuming a symmetric matric A
+L = norm(xx,'fro')^2;    
 
 end
